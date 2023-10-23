25bed6ce337ab4ee26fd665ab14a5d40b2732b7d
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2023-10-23 12:30:01 +0100

    prevent scrolling while the toggle displayed

--- scripts.js
+++ scripts.js
@@ -22,13 +22,16 @@ function message(m) {
 const nav = document.querySelector(".nav-mobile");
 const menu = document.querySelector(".menu");
 const close = document.querySelector(".close");
+const body = document.querySelector("body");
 
 menu.addEventListener("click", () => {
   nav.classList.add("nav-mobile-visible");
   close.classList.add("close-show");
+  body.classList.add("body-no-scroll");
 });
 
 close.addEventListener("click", () => {
   nav.classList.remove("nav-mobile-visible");
   close.classList.remove("close-show");
+  body.classList.remove("body-no-scroll");
 });
